94c764d955f17cdde3ddb5adbe25de9b4503560b
Masa-FF <na7stea2@gmail.com>
2022-02-25 23:27:39 +0900

    Basic grammar JavaScript

---
+++ #2_詳解JavaScript基礎文法編/Readme.md
@@ -0,0 +1,10 @@
+ドットインストール<br>
+#2 詳解JavaScript 基礎文法編<br>
+
+#01 JavaScriptを学ぼう<br>
+#02 はじめてのJavaScript<br>
+#03 別ファイルから実行してみよう<br>
+#04 エラーを修正してみよう<br>
+#05 文字列を扱ってみよう<br>
+
+https://dotinstall.com/lessons/basic_javascript_grammer_v2/52202
---
+++ #2_詳解JavaScript基礎文法編/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('Hello World from main.js!');
+// console.log('Hello');
+// console.log('Hello');
+
+// console.log("it's me!");
+// console.log('it\'s me!');
+
+console.log('Hello\nWorld\t!');
+console.log('Hello'+'World!')
+
+// \n　改行
+// \t　タブ
+
+// 文字列の表現
+// \の使い方
+// 文字列の連結
