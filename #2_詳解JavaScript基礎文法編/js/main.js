491ae7af2814fa53b8f8e8a6ac78030fc87b61a0
Masa-FF <na7stea2@gmail.com>
2022-02-26 14:33:53 +0900

    basic_javascript_grammer_v2

--- #2_詳解JavaScript基礎文法編/js/main.js
+++ #2_詳解JavaScript基礎文法編/js/main.js
@@ -7,8 +7,8 @@ console.log('Hello World from main.js!');
 // console.log("it's me!");
 // console.log('it\'s me!');
 
-console.log('Hello\nWorld\t!');
-console.log('Hello'+'World!')
+// console.log('Hello\nWorld\t!');
+// console.log('Hello'+'World!');
 
 // \n　改行
 // \t　タブ
@@ -16,3 +16,17 @@ console.log('Hello'+'World!')
 // 文字列の表現
 // \の使い方
 // 文字列の連結
+
+console.log(10 + 3); //13
+console.log(10 - 3); //7
+console.log(10 * 3); //30
+console.log(10 % 3);
+console.log(10 / 3); //3.3333...
+console.log(10 ** 3); //1000
+console.log(10 + 2 * 3); //16
+console.log((10 + 2) * 3); //36
+
+const price = 100;
+
+console.log(price * 120);
+console.log(price * 150);
