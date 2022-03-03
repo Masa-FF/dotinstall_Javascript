0c7e42b29dec21fc4fe31923936efb93dd1b8d16
Masa-FF <na7stea2@gmail.com>
2022-03-03 20:40:54 +0900

    #20 関数で処理をまとめてみよう

--- #2_詳解JavaScript基礎文法編/js/main.js
+++ #2_詳解JavaScript基礎文法編/js/main.js
@@ -160,3 +160,18 @@ for (let a = 1; a <= 10; a++){
   }
   console.log(a)
 }
+
+//#20 関数で処理をまとめてみよう
+function FizzBuzz(){
+  for(let i = 1; i <=15; i++ ){
+    if (i % 15 === 0)
+      console.log('FizzBuzz');
+    else if (i % 5 === 0)
+      console.log('Buzz');
+    else if (i % 3 === 0)
+      console.log('Fuzz');
+    else
+      console.log(i);
+  }
+}
+FizzBuzz();
