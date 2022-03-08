5261f3fbe259a87f7200b407468ceb938d2f53d4
Masa-FF <na7stea2@gmail.com>
2022-03-08 22:27:06 +0900

    #02配列の要素にアクセスしてみよ

--- #3_詳解JavaScript オブジェクト編/js/main.js
+++ #3_詳解JavaScript オブジェクト編/js/main.js
@@ -1,4 +1,12 @@
 'use strict';
 
-const scores = [10, 100, 100 ];
-console.log(scores);
+{
+const scores = [10, 100, 100];
+// console.log(scores[1]);
+scores[2] = 500;
+console.log(scores[2])
+console.log(scores.length)
+}
+
+//定数で指定した値でも、入れるの数値へ代入することは可能
+
