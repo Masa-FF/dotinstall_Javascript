f0351009429489bf0acd48d07c1bc2976afc42e0
Masa-FF <na7stea2@gmail.com>
2022-03-08 22:39:11 +0900

    #03 配列とループ処理を組み合わせよう

--- #3_詳解JavaScript オブジェクト編/js/main.js
+++ #3_詳解JavaScript オブジェクト編/js/main.js
@@ -10,3 +10,17 @@ console.log(scores.length)
 
 //定数で指定した値でも、入れるの数値へ代入することは可能
 
+// #03 配列とループ処理を組み合わせよう
+
+// {
+//   const scores = [80, 90 ,100];
+//   console.log(`Score: ${scores[0]}`)
+//   console.log(`Score: ${scores[1]}`)
+//   console.log(`Score: ${scores[2]}`)
+// }
+
+const myScores = [80, 90, 100];
+// for (let i = 0; i < 3; i++){
+for (let i = 0; i < myScores.length; i++){
+  console.log(`Score${i}: ${myScores[i]}`);
+}
